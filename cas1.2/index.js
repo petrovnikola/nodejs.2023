6a1dbe284061386f657076aabd9215a1c7ac988d
petrovnikola <115044256+petrovnikola@users.noreply.github.com>
2023-02-15 21:08:14 +0100

    commit c2

---
+++ cas1.2/index.js
@@ -0,0 +1,34 @@
+// console.log("test");
+// console.log(5 != 5);
+// console.log(typeof 25);
+
+// const chovek = {
+//   name: "John",
+//   surname: "Doe",
+// };
+// console.log(chovek);
+// let niza = [1, true, 2.5, "zdravo"];
+// console.log(niza);
+
+// student = {
+//   ime: "pero",
+//   prezime: "nakov",
+//   age: 25,
+// };
+
+// console.log(student);
+// student = {
+//   ime: "zivko",
+// };
+// console.log(student);
+
+function testFN() {
+  return "test fn";
+}
+console.log(testFN());
+testFN();
+
+const TESTFN = () => {
+  console.log("test fn 1 ");
+};
+TESTFN();
